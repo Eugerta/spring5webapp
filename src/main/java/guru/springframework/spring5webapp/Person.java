bd9b67d03b0bfa6495d53c985370d1d54a3fdd3b
Eugerta Haris <eugerta.haris@moveoalbania.com>
2021-04-26 17:04:59 +0200

    Added Person object

---
+++ src/main/java/guru/springframework/spring5webapp/Person.java
@@ -0,0 +1,28 @@
+package guru.springframework.spring5webapp;
+
+public class Person {
+
+    private String name;
+    private String lName;
+
+    public Person(String name, String lName) {
+        this.name = name;
+        this.lName = lName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+}
