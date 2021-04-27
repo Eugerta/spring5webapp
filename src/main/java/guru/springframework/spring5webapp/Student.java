6e16c431f518703a35a45ba6a78edc1e88100ad7
Eugerta Haris <eugerta.haris@moveoalbania.com>
2021-04-27 10:29:02 +0200

    Added Student property

--- src/main/java/guru/springframework/spring5webapp/Student.java
+++ src/main/java/guru/springframework/spring5webapp/Student.java
@@ -1,2 +1,28 @@
-package guru.springframework.spring5webapp;public class Student {
+package guru.springframework.spring5webapp;
+
+public class Student {
+
+    private String student_id;
+    private String name;
+
+    public Student(String student_id, String name) {
+        this.student_id = student_id;
+        this.name = name;
+    }
+
+    public String getId() {
+        return student_id;
+    }
+
+    public void setId(String id) {
+        this.student_id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
